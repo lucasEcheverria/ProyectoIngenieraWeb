1854efb68085a7442be788e2634bd0fbf99e072e
lucasEcheverria <lucas.echeverria@opendeusto.es>
2024-10-31 09:39:28 +0100

    views django

--- Deustify/spotyWebApp/urls.py
+++ Deustify/spotyWebApp/urls.py
@@ -6,9 +6,9 @@ from . import views
 urlpatterns = [
     path('',views.index,name = 'index'),
     path('generos',views.listaGeneros, name= 'listaGeneros'),
-    path('generos/<int:id_genero>/', views.detalleGeneros, name='detalleGenero'),
+    path('generos/<int:id_genero>/', views.detalleGenero, name='detalleGenero'),
     path('canciones',views.listaCanciones, name= 'listaCanciones'),
     path('canciones/<int:id_cancion>/',views.detalleCancion, name= 'detalleCancion'),
     path('artistas',views.listaArtistas, name= 'listaArtistas'),
-    path('artistas/<int:id_artista>/', views.detalleArtistas, name='detalleArtista'),
+    path('artistas/<int:id_artista>/', views.detalleArtista, name='detalleArtista'),
 ]
--- Deustify/spotyWebApp/views.py
+++ Deustify/spotyWebApp/views.py
@@ -13,29 +13,27 @@ def listaGeneros(request):
     contexto = {'generos' : generos}
     return render(request, 'listaGenero.html', contexto)
 
-def detalleGeneros(request, id_genero):
+def detalleGenero(request, id_genero):
     genero = get_object_or_404(Genero, pk=id_genero)
     contexto = {'genero': genero}
     return render(request, 'detalleGenero.html', contexto)
 
 def listaArtistas(request):
     artistas = Artista.objects.order_by('nombre')
-    nombre_artistas = ', '.join([artista.nombre for artista in artistas])
-    return HttpResponse(nombre_artistas)
+    contexto = {'artistas' : artistas}
+    return render(request, 'listaArtista.html', contexto)
 
-def detalleArtistas(request, id_artista):
+def detalleArtista(request, id_artista):
     artista = get_object_or_404(Artista,pk=id_artista)
     contexto = {'artista':artista}
     return render(request,'detalleArtista.html',contexto)
+
 def listaCanciones(request):
-    canciones = Cancion.objects.order_by('id')
-    nombre_canciones = ', '.join([cancion.nombre for cancion in canciones])
-    return HttpResponse(nombre_canciones)
+    canciones = Cancion.objects.order_by('nombre')
+    contexto = {'canciones' : canciones}
+    return render(request, 'listaCanciones.html', contexto)
 
 def detalleCancion(request,id_cancion):
-    try:
-        cancion = Cancion.objects.get(pk = id_cancion)
-        cadenaDeTexto = f"{cancion.nombre} - ID: {cancion.fechaLanzamiento} - Artista: {cancion.artista}\n"
-        return HttpResponse(cadenaDeTexto)
-    except cancion.DoesNotExist:
-        return HttpResponseNotFound("Cancion no encontrada")
+    cancion = get_object_or_404(Artista,pk=id_cancion)
+    contexto = {'cancion':cancion}
+    return render(request,'detalleCancion.html',contexto)
